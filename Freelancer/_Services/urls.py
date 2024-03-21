8545709d51b38d4e3c22168096ca77a7f05245cd
Guitta Moubarak <guittamoubarak@gmail.com>
2024-03-21 14:34:34 +0200

    modified the home page and base.html, created the search_results page still needs improvements

--- Freelancer/_Services/urls.py
+++ Freelancer/_Services/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = '_Services'
 
 urlpatterns = [
-    path('', views.search_service, name='search_service')
+   path('', views.search_service, name='search_service'),
+   path('search_results', views.search_service, name='search_results')
+   
     # path('signup', views.signup, name='signup'),
 ]
--- Freelancer/_Services/views.py
+++ Freelancer/_Services/views.py
@@ -15,3 +15,5 @@ def search_service(request):
         services = Service.objects.filter(
             service_type__code_type=service_type_code, service_location=area_code)
         return render(request, '_Services/search_results.html', {'services': services})
+
+
