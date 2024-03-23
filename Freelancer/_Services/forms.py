710e7487f186c49a32ad99d6947936fb2b57f12d
Guitta Moubarak <guittamoubarak@gmail.com>
2024-03-23 20:52:47 +0200

    edited the interface of add service

--- Freelancer/_Services/forms.py
+++ Freelancer/_Services/forms.py
@@ -14,4 +14,13 @@ class AddServiceForm(forms.ModelForm):
         model = Service
         fields = ['service_title', 'service_desc',
                   'service_type', 'service_location', 'images']
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['service_title'].widget.attrs['class'] = 'form-control border border-dark border-1'
+        self.fields['service_title'].label = 'Service Title'
+        self.fields['service_desc'].widget.attrs['class'] = 'form-control border border-dark border-1'
+        self.fields['service_desc'].label = 'Service Description'
+        self.fields['service_type'].widget.attrs['class'] = 'form-control border border-dark border-1'
+        self.fields['service_location'].widget.attrs['class'] = 'form-control border border-dark border-1'
+        self.fields['images'].widget.attrs['class'] = 'form-control border border-dark border-1'
+        self.fields['images'].label = 'Service Images'
